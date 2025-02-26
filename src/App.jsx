87aaac0f55d906f6420e0bf440d9ecbf25b094ab
kkirrin <khlebnikov-2016@mail.ru>
2025-02-26 10:01:50 +1000

    feat: сделал моковые данные

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import { Loading } from './components';
+import { Suspense, useState, useEffect } from 'react';
+import { Loading, MainComponent } from './components';
 
-function App({ children }) {
-  return (
-    <div className='page-wrapper'>
-      <Loading />
-      <main>
-        {children}
-      </main>
-    </div>
-  )
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export default App
+function App() {
+  const [data, setData] = useState([]);
+  const [dataLoading, setDataLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Иммитация загрузки
+      await delay(2000);  
+
+      // Моковые данные
+      const mockData = [
+        { id: 1, name: 'АО "Находкинский морской торговый порт" (УТ-1)', description: 'Description 1' },
+        { id: 2, name: 'АО "Находкинский морской торговый порт" (ГУТ-2)', description: 'Description 2' },
+        { id: 3, name: 'АО "Порт Вера"', description: 'Description 3' },
+        { id: 4, name: 'ООО "Морской Порт "Суходол"', description: '' },
+        { id: 5, name: 'Дробильные установки', description: '' },
+        { id: 6, name: 'Техника', description: '' },
+      ];
+
+      setData(mockData);
+      setDataLoading(false); 
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className='page-wrapper'>
+      {/* Suspense пригодится когда будет асинхронный запрос, или нет хз */}
+      <Suspense fallback={<Loading />}>
+        <main>
+          {dataLoading ? (  
+            <Loading />
+          ) : (
+            <MainComponent data={data} /> 
+          )}
+        </main>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import styles from './style.module.scss';
 
-const Loading = () => {
+
+export default function Loading() {
   return (
     <section className={styles.main_bg}>
-      <div className={`${styles.wrapper} container`}>
-        <div className={styles.info_text}>
-            Отчеты
+        <div className={`${styles.wrapper} container`}>
+          <div className={styles.info_text}>
+              Отчеты
+          </div>
+          <h2 className={styles.title}>Морской порт</h2>
+          <div className={styles.loader}>
+          </div>
         </div>
-        <h2 className={styles.title}>Морской порт</h2>
-        <div className={styles.loader}></div>
-      </div>
-      
-      <div className={styles.volna}>
-            <img src='/volna.png' alt='' />
+
+        <div className={styles.volna}>
+              <img src='/volna.png' alt='' />
         </div>
     </section>
   );
 };
 
-export default Loading;
