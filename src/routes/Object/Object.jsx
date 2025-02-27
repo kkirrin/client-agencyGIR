1fd137480e2e8f04f880440defc93cc8790ae9b2
kkirrin <khlebnikov-2016@mail.ru>
2025-02-27 16:37:50 +1000

    feat: добавил func daysInMonth

--- src/routes/Object/Object.jsx
+++ src/routes/Object/Object.jsx
@@ -3,53 +3,36 @@ import { useParams } from 'react-router-dom';
 import styles from './style.module.scss';
 import { AddWorkerBtn, ComponentDate, ComponentSearch } from '../../components';
 
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
 
 const Object = () => {
   const { id } = useParams();
 
+  const object = { id: 1, name: 'АО "Находкинский морской торговый порт" (УТ-1)' }; 
 
-  // Вероятно, по этому id будет снова запрос, пока данные замокаю
-  const mockData = {
-    "data": {
-      "id": 11,
-      "documentId": "yxon55d950jiowczmrhysc4x",
-      "createdAt": "2025-01-29T23:41:41.342Z",
-      "updatedAt": "2025-02-04T00:12:38.701Z",
-      "objects": [
-        {
-          'name': 'АО "Находкинский морской торговый порт" (УТ-1)',
-        }
-      ]
-    }
-  };
-
-  const object = mockData.data.objects.find(obj => obj.name); 
-
-  if (!object) {
-    return (
-      <section className={styles.main_section}>
-        <div className="container">
-          <p>Объект не найден</p>
-        </div>
-      </section>
-    );
-  }
+  const numDays = daysInMonth(2, 2025);
+  const days = Array.from({ length: numDays }, (_, i) => i + 1);
 
   return (
     <section className={styles.main_section}>
       <div className="container">
         <div className={styles.top}>
           <p className={styles.name_object}>{object.name}</p>
-          
-
           <div className={styles.top_wrapper}>
             <ComponentSearch />
             <ComponentDate />
           </div>
         </div>
-
-        <div className={styles.table}></div>
-
+        <div className={styles.table}>
+          <div className={styles.table_header}>
+            <p>ФИО/должность</p>
+            {days.map((day) => (
+              <p key={day}>{day}</p>
+            ))}
+          </div>
+        </div>
         <div className={styles.add_workers}>
           <AddWorkerBtn />
         </div>
@@ -59,4 +42,5 @@ const Object = () => {
 };
 
 export default Object;
+
 
