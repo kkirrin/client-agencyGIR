7608f132076981a88456769341d404796b9290a2
kkirrin <khlebnikov-2016@mail.ru>
2025-02-28 15:44:29 +1000

    feat: пока заполняем тестовой версткой

--- src/routes/Object/Object.jsx
+++ src/routes/Object/Object.jsx
@@ -50,8 +50,10 @@ const Object = () => {
 
         <div className={styles.table}>
           <div className={styles.table_header}>
+
             <ul className={`${styles.day_list} ${styles.wrapper_day}`}>
-              {/* <p className={styles.title_table}>ФИО/должность</p> */}
+                  {/* <p className={styles.title_table}>ФИО/должность</p> */}
+
               {displayedDays.map((day) => (
                 <li className={styles.item_table} key={day}>
                   <div>{day}</div>
@@ -82,21 +84,119 @@ const Object = () => {
 
         <div className={styles.workers_list}>
           {workers.map((worker) => (
-            <div className={styles.workers_item} key={worker.id}> 
-              <div className={styles.worker_data}>
-                <p>{worker.name}</p>
-                <p>Тоннаж</p>
+            <div key={worker.id} className={styles.workers_item_wrapper}>
+              <div className={styles.workers_item} > 
+                <div className={styles.worker_data}>
+                  <p>{worker.name}</p>
+                  <p>Тоннаж</p>
+                </div>
+
+                {displayedDays.map((day) => (
+                  <div className={styles.item_table} key={day}>
+                    <div className={styles.item_data}>
+                      <div className={styles.detail}> 
+                        <p>
+                          Тоннаж 
+                        </p>
+
+                        <p>
+                          500,25
+                        </p>
+                      </div>
+
+                      <div className={styles.detail}>
+                        <p>
+                          ТС 
+                        </p>
+
+                        <p>
+                          УСМ-20
+                        </p>
+                      </div>
+
+                      <div>
+                        <p>
+                          Примечание
+                        </p>
+                      </div>
+                    </div>
+                    <div className='border_top_gray'>
+
+                    </div>
+                    <div className={styles.item_data}>
+                      <div className={styles.detail}> 
+                        <p>
+                          не работал
+                        </p>
+                      </div>
+  
+                      <div>
+                        <p>
+                          Примечание
+                        </p>
+                      </div>
+                    </div>
+
+                  </div>
+                ))}
+
+                <button onClick={() => {}}>
+                  <img src='/edit.svg' alt='' /> 
+                </button>
               </div>
 
-              {displayedDays.map((day) => (
-                <p className={styles.item_table} key={day}>
-                  Тут будут данные
-                </p>
-              ))}
-              <button onClick={() => {}}>
-                <img src='/edit.svg' alt='' /> 
-              </button>
+              <div className={styles.sum}>
+                <div className={styles.sum_wrapper}>
+                  <p className={styles.sum_text}>Всего</p>
+                  <p className={styles.sum_month}>Месяц</p>
+                </div>
+
+                <div className={styles.sum_detail}>
+                  <p>Дневные смены</p>
+                  <p>9</p>
+                </div>
+
+                <div className={styles.sum_detail}>
+                  <p>Ночные смены</p>
+                  <p>9</p>
+                </div>
+
+
+                <div className={styles.sum_detail}>
+                  <p>Всего смен</p>
+                  <p>19</p>
+                </div>
+
+
+                <div className={styles.sum_detail}>
+                  <p>Общий тоннаж</p>
+                  <p>9000,89</p>
+                </div>
+
+                <div className={styles.sum_detail}>
+                  <p>Выставили</p>
+                  <p>9000,89</p>
+                </div>
+
+                <div className={styles.sum_detail}>
+                    <p>Выставили</p>
+                    <p>
+                      10 000,89
+                    </p>
+                </div>
+
+                <div className={styles.sum_detail}>
+                  <p>Ост. Порт</p>
+                  <p>1000,89</p>
+                </div>
+
+                <div className={styles.sum_detail}>
+                  <p>Ост. ГиР</p>
+                  <p>1500,89</p>
+                </div>
+              </div>  
             </div>
+            
           ))}
         </div>
 
