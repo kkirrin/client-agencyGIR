f5f8e9af8e1849ead176da8885e7decdffbf994d
Сергей Субботин <zarodiny@yandex.ru>
2025-03-27 12:29:45 +1000

    feat: добавляет стили

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -25,6 +25,11 @@ export default function MainComponent({ data, domain }) {
                   <p className={styles.item_title}>
                     {item.Name ? item.Name : 'Ошибка получения заголовка'}
                   </p>
+                  <svg className={styles.arrow} width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.0914 17.3539L16.7844 5.66089" stroke="#253360" strokeWidth="2" />
+                    <path d="M6.60146 5.66095H17.9233" stroke="#253360" strokeWidth="2" />
+                    <path d="M16.9219 16.2776V4.95575" stroke="#253360" strokeWidth="2" />
+                  </svg>
                 </Link>
               </li>
             );
